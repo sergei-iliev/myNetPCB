162f9894902de81abf6551364f45bfb0811d73ca
sergei.iliev <sergei.iliev@gmail.com>
2017-09-05 07:45:03 +0200

    font

--- core/src/com/mynetpcb/core/capi/text/glyph/GlyphManager.java
+++ core/src/com/mynetpcb/core/capi/text/glyph/GlyphManager.java
@@ -35,7 +35,7 @@ public enum GlyphManager {
     
     private void initialize() throws Exception{
         
-        try(InputStream istream =getClass().getResourceAsStream("defaultfont.xml")){
+        try(InputStream istream =getClass().getResourceAsStream("/com/mynetpcb/core/fonts/defaultfont.xml")){
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         DocumentBuilder builder = factory.newDocumentBuilder();
         InputSource is = new InputSource(istream);
