5365a1632424a9838553d0f7330a24a6998bc91d
sergei_iliev <sergei.iliev@gmail.com>
2020-10-29 08:35:14 +0200

    start circuit

--- core/src/com/mynetpcb/core/capi/pin/CompositePinable.java
+++ core/src/com/mynetpcb/core/capi/pin/CompositePinable.java
@@ -1,5 +1,6 @@
 package com.mynetpcb.core.capi.pin;
 
+import com.mynetpcb.d2.shapes.Box;
 import com.mynetpcb.d2.shapes.Point;
 
 import java.util.Collection;
@@ -7,4 +8,8 @@ import java.util.Collection;
 public interface CompositePinable {
     
     public Collection<Point> getPinPoints();
+    
+    public default Box getPinsRect(){
+        return null;
+    }
 }
